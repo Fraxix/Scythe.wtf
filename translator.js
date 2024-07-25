e6d22b2f99ee0a744f49747ee0afd41e0713d732
Ray <RayShiba@proton.me>
2024-07-25 10:38:14 +0200

    modified:   translator.js

--- translator.js
+++ translator.js
@@ -1,18 +1,8 @@
 const translations = {
     en: {
-        Title: "Welcome to the website!",
-        description: "This is a sample description.",
-        Card1Desc: "A fully universal internal DX11 base w/ ImGui",
-        ViewGithubButton: "View Github",
-        Card2Desc: "Literally this site. Although still under construction",
         Message: "Website under construction!"
     },
     ru: {
-        Title: "Добро пожаловать на сайт!",
-        description: "Это пример описания.",
-         Card1Desc: "Полностью универсальная внутренняя база DX11 с ImGui.",
-         ViewGithubButton: "Посмотреть Гитхаб",
-         Card2Desc: "Буквально этот сайт. Хотя еще в стадии строительства",
           Message: "Сайт в разработке!"
     }
 };
